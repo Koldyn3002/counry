e5af027865a70d87694d54fef8c17657b9f7f5cb
Владислав <95358862+Koldyn3002@users.noreply.github.com>
2024-07-30 15:11:25 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,72 +1,67 @@
-$(document).ready(function () {
-  // Запрос к API для получения списка стран
-  $.ajax({
-    url: 'https://restcountries.com/v3.1/all',
-    dataType: 'json',
-    success: function (data) {
-      // Обработка успешного запроса
-      let countries = data;
-      let countriesList = $('#countries-list');
-      let searchInput = $('#search-input');
-      let searchResults = $('#search-results');
 
-      // Функция для фильтрации стран по имени
-      function filterCountries(query) {
-        let filteredCountries = countries.filter(function (country) {
-          return country.name.common
-            .toLowerCase()
-            .includes(query.toLowerCase());
-        });
-        return filteredCountries;
-      }
+$(document).ready(function() {
+    // Запрос к API для получения списка стран
+    $.ajax({
+        url: "https://restcountries.com/v3.1/all",
+        dataType: "json",
+        success: function(data) {
+            // Обработка успешного запроса
+            let countries = data;
+            let countriesList = $("#countries-list");
+            let searchInput = $("#search-input");
+            let searchResults = $("#search-results");
 
-      // Обработка ввода в поисковую строку
-      searchInput.on('input', function () {
-        let query = $(this).val();
-        let filteredCountries = filterCountries(query);
-        searchResults.empty();
+            // Функция для фильтрации стран по имени
+            function filterCountries(query) {
+                let filteredCountries = countries.filter(function(country) {
+                    return country.name.common.toLowerCase().includes(query.toLowerCase());
+                });
+                return filteredCountries;
+            }
 
-        if (filteredCountries.length > 0) {
-          filteredCountries.forEach(function (country) {
-            let countryItem = $('<div>')
-              .addClass('country-item')
-              .text(country.name.common);
-            countryItem.click(function () {
-              // Обработка клика по стране
-              showCountryDetails(country);
+            // Обработка ввода в поисковую строку
+            searchInput.on("input", function() {
+                let query = $(this).val();
+                let filteredCountries = filterCountries(query);
+                searchResults.empty();
+
+                if (filteredCountries.length > 0) {
+                    filteredCountries.forEach(function(country) {
+                        let countryItem = $("<div>").addClass("country-item").text(country.name.common);
+                        countryItem.click(function() {
+                            // Обработка клика по стране
+                            showCountryDetails(country);
+                        });
+                        searchResults.append(countryItem);
+                    });
+                } else {
+                    searchResults.text("Ничего не найдено");
+                }
             });
-            searchResults.append(countryItem);
-          });
-        } else {
-          searchResults.text('Ничего не найдено');
+
+            // Отображение всех стран
+            countries.forEach(function(country) {
+                let countryItem = $("<div>").addClass("country-item").text(country.name.common);
+                countryItem.click(function() {
+                    // Обработка клика по стране
+                    showCountryDetails(country);
+                });
+                countriesList.append(countryItem);
+            });
+        },
+        error: function() {
+            // Обработка ошибки запроса
+            alert("Ошибка при загрузке списка стран");
         }
-      });
+    });
 
-      // Отображение всех стран
-      countries.forEach(function (country) {
-        let countryItem = $('<div>')
-          .addClass('country-item')
-          .text(country.name.common);
-        countryItem.click(function () {
-          // Обработка клика по стране
-          showCountryDetails(country);
-        });
-        countriesList.append(countryItem);
-      });
-    },
-    error: function () {
-      // Обработка ошибки запроса
-      alert('Ошибка при загрузке списка стран');
-    },
-  });
-
-  // Функция для отображения подробной информации о стране
-  function showCountryDetails(country) {
-    let countryDetails = $('#country-details');
-    countryDetails.empty();
-    let countryName = $('<h2>').text(country.name.common);
-    let countryCapital = $('<p>').text('Capital: ' + country.capital[0]);
-    let countryFlag = $('<img>').attr('src', country.flags.png);
-    countryDetails.append(countryName, countryCapital, countryFlag);
-  }
+    // Функция для отображения подробной информации о стране
+    function showCountryDetails(country) {
+        let countryDetails = $("#country-details");
+        countryDetails.empty();
+        let countryName = $("<h2>").text(country.name.common);
+        let countryCapital = $("<p>").text("Capital: " + country.capital[0]);
+        let countryFlag = $("<img>").attr("src", country.flags.png);
+        countryDetails.append(countryName, countryCapital, countryFlag);
+    }
 });
